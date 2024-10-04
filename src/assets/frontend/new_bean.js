41db2069ca43f4047bdb21e7874afbc9d0aaa70f
nguyentanphatt <bloodomega97@gmail.com>
2024-10-04 13:41:42 +0700

    commit

---
+++ src/assets/frontend/new_bean.js
@@ -0,0 +1,39 @@
+import image01 from './Geisha.jpg'
+import image02 from './matcha.jpg'
+import image03 from './Hibiscus_seed.jpg'
+let new_bean = [
+    {
+        "id":1,
+        "name": "Geisha",
+        "price": 6.99,
+        "description": "Geisha (or Gesha) coffee is known for its extraordinary aromatic complexity, often featuring jasmine, tropical fruit, and floral notes. Originating in Ethiopia and now popular in Panama, it’s considered one of the most premium and flavorful coffee varieties.",
+        "small-descripthon": "Highly aromatic with jasmine",
+        "image": image22,
+        "category": "Bean and Seed",
+        "quantity": 100,
+        "type":"Coffee bean"
+    },
+    {
+        "id":2,
+        "name": "Matcha",
+        "price": 3.99,
+        "description": "Matcha is a powdered green tea from Japan, known for its vibrant green color and concentrated antioxidants. It provides a smooth, rich flavor and is packed with health benefits, including energy-boosting and detoxifying properties.",
+        "small-descripthon": " A powdered green tea",
+        "image": image26,
+        "category": "Bean and Seed",
+        "quantity": 100,
+        "type":"Tea leaf"
+    },
+    {
+        "id":23,
+        "name": "Hibiscus",
+        "price": 9.99,
+        "description": "Hibiscus tea is made from the dried petals of the hibiscus flower. It has a tart, cranberry-like flavor and is known for lowering blood pressure, supporting heart health, and aiding in digestion.",
+        "small-descripthon": "Tart, cranberry-like flavor",
+        "image": image27,
+        "category": "Bean and Seed",
+        "quantity": 100,
+        "type":"Tea leaf"
+    },
+]
+export default new_bean;
