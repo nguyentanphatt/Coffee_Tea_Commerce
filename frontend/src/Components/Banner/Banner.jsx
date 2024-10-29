b7972567a8b31dcadf40bc3bd94bd88faa7bcd17
nguyentanphatt <bloodomega97@gmail.com>
2024-10-29 12:53:33 +0700

    add MUI for Banner

--- frontend/src/Components/Banner/Banner.jsx
+++ frontend/src/Components/Banner/Banner.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 import banner_img from '../../assets/frontend/main_banner.webp'
+import { Button, Typography } from '@mui/material'
 import './Banner.css'
 const Banner = () => {
   return (
     <div className='banner'>
         <div className='banner-left'>
-            <h1>BEST COFFEE IN THE WORLD</h1>
-            <p>Get now to sale up to 20%</p>
-            <button>Get it now!!</button>
+            <Typography
+              variant='h3'
+              sx={{
+                color: '#fff',
+                mb: 2,
+                fontWeight: '600'
+              }}
+            >BEST COFFEE IN THE WORLD</Typography>
+            <Typography
+              variant='h6'
+              sx={{
+                mb: 2,
+                color: '#fff'
+              }}
+            >Get now to sale up to 20%</Typography>
+            <Button
+              variant='contained'
+              size='large'
+              sx={{
+                fontSize: 16,
+                fontWeight: '600',
+                width: '180px',
+                backgroundColor: '#342B2B',
+                '&:hover':{
+                  backgroundColor: 'gray',
+                  color: '#342B2B'
+                }
+              }}
+            >Get it now!!</Button>
         </div>
         <div className="banner-right">
             <img src={banner_img} alt="" />
