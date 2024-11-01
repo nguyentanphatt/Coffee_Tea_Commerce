76d53a9f5d0e81bf2e1b6b87ea82ba1b4decebaa
nguyentanphatt <bloodomega97@gmail.com>
2024-11-01 14:43:45 +0700

    add MUI Breadcrums

--- frontend/src/Context/ScrollToTop.jsx
+++ frontend/src/Context/ScrollToTop.jsx
@@ -1,14 +1,56 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
-
+import { Fab } from '@mui/material';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 const ScrollToTop = () => {
 
-    const {pathname} = useLocation()
-    useEffect(()=>{
-        window.scrollTo(0,0)
-    },[pathname])
+  const { pathname } = useLocation();
+  const [showButton, setShowButton] = useState(false);
 
-  return null;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <>
+      {showButton && (
+        <Fab
+          color="primary"
+          size='small'
+          onClick={scrollToTop}
+          sx={{
+            position: 'fixed',
+            bottom: 16,
+            right: 16,
+          }}
+        >
+          <ArrowUpwardIcon />
+        </Fab>
+      )}
+    </>
+  );
 }
 
 export default ScrollToTop
